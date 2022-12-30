edd4cf8ef2cb550fea7a84eabf13ac788db9f4c1
ItzOver <alireza@daneshian.ir>
2022-12-30 06:33:32 +0330

    fixed addon

--- src/main/java/me/overlight/powertools/AddOns/AddOn.java
+++ src/main/java/me/overlight/powertools/AddOns/AddOn.java
@@ -6,7 +6,6 @@ import org.bukkit.configuration.file.FileConfiguration;
 public class AddOn {
     private String name, version, description, perm;
     private boolean enabled;
-    public int startDelay, delay;
     public FileConfiguration config;
     public PowerTools plugin;
     public AddOn(String name, String version, String description, String requiredPerm, boolean stats){
@@ -18,18 +17,6 @@ public class AddOn {
         this.plugin = PowerTools.INSTANCE;
         this.config = this.plugin.getConfig();
         if(stats) onEnabled();
-    }
-    public AddOn(String name, String version, String description, String requiredPerm, int startDelay, int delay, boolean stats){
-        this.name = name;
-        this.version = version;
-        this.description = description;
-        this.perm = requiredPerm;
-        this.startDelay = startDelay;
-        this.delay = delay;
-        this.enabled = stats;
-        this.plugin = PowerTools.INSTANCE;
-        this.config = this.plugin.getConfig();
-        if(stats) this.onEnabled();
     }
 
     public void onEnabled() { }
