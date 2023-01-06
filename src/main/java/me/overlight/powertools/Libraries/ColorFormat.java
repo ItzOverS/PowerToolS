213582ab8a74238b45fb92d0495fe18b2da610ae
ItzOver <alireza@daneshian.ir>
2023-01-06 19:07:49 +0330

    fixed ColorFormat.java

--- src/main/java/me/overlight/powertools/Libraries/ColorFormat.java
+++ src/main/java/me/overlight/powertools/Libraries/ColorFormat.java
@@ -7,7 +7,7 @@ public class ColorFormat {
         String ms = msg;
         // Chat Colors
         ms = ms.replace("@color_black", "&0").replace("@color_dark_blue", "&1")
-                .replace("@color_green", "&2").replace("@color_dark_aqua", "&3")
+                .replace("@color_dark_green", "&2").replace("@color_dark_aqua", "&3")
                 .replace("@color_dark_red", "&4").replace("@color_dark_purple", "&5")
                 .replace("@color_gold", "&6").replace("@color_gray", "&7")
                 .replace("@color_dark_gray", "&8").replace("@color_blue", "&9")
