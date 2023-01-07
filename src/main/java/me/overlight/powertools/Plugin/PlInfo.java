63dc86a0bad39e5fefead55beaf56c3b5ae36059
ItzOver <alireza@daneshian.ir>
2023-01-07 14:51:13 +0330

    fixed KICK_PREFIX

--- src/main/java/me/overlight/powertools/Plugin/PlInfo.java
+++ src/main/java/me/overlight/powertools/Plugin/PlInfo.java
@@ -4,7 +4,7 @@ import org.bukkit.ChatColor;
 
 public class PlInfo {
     public static final String PREFIX = ChatColor.translateAlternateColorCodes('&', "&6[&l&2Power&b&lToolS&r&6]&r&f ");
-    public static final String KICK_PREFIX = ChatColor.translateAlternateColorCodes('&', "&5 POWER TOOLS\n\\___________________/");
+    public static final String KICK_PREFIX = ChatColor.translateAlternateColorCodes('&', "&5 POWER TOOLS\n\\___________________/\n");
     public static final String VERSION = "1.0";
 
     public static final class ADDONS{
