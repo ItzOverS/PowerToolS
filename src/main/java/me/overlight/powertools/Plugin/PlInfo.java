44528cd0ccd40f3d6184002b067184f30c2169f3
ItzOver <alireza@daneshian.ir>
2023-01-20 11:48:45 +0330

    updated to v1.3

--- src/main/java/me/overlight/powertools/Plugin/PlInfo.java
+++ src/main/java/me/overlight/powertools/Plugin/PlInfo.java
@@ -5,7 +5,7 @@ import org.bukkit.ChatColor;
 public class PlInfo {
     public static final String PREFIX = ChatColor.translateAlternateColorCodes('&', "&6[&l&2Power&b&lToolS&r&6]&r&f ");
     public static final String KICK_PREFIX = ChatColor.translateAlternateColorCodes('&', "&5 POWER TOOLS\n\\___________________/\n");
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
 
     public static final class ADDONS{
         public static final String SurvivalPrefix = ChatColor.translateAlternateColorCodes('&', "&6[&l&2Power&b&lSurvivalS&r&6]&r&f ");
