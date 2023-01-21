6a97895b4ce1797e0a0f2045706c5598f52ffe74
ItzOver <alireza@daneshian.ir>
2023-01-21 21:05:39 +0330

    added Captcha addon

---
+++ src/main/java/me/overlight/powertools/AddOns/Main/Captcha/MapViewRenderer.java
@@ -0,0 +1,34 @@
+package me.overlight.powertools.AddOns.Main.Captcha;
+
+import org.bukkit.entity.Player;
+import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapPalette;
+import org.bukkit.map.MapRenderer;
+import org.bukkit.map.MapView;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+public class MapViewRenderer
+        extends MapRenderer {
+    boolean done = false;
+    @Override
+    public void render(MapView mapView, MapCanvas mapCanvas, Player player) {
+        if(done)
+            return;
+        Random random = new Random();
+        BufferedImage img = new BufferedImage(64, 64, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = img.createGraphics();
+        String num = "";
+        for(int i = 0; i < 3; i++){
+            int rand = random.nextInt(10);
+            num += rand;
+            g.drawString(rand + "", (i + 1) * (random.nextInt(5) * 2), (i + 1) * random.nextInt(10));
+        }
+        g.dispose();
+        img = MapPalette.resizeImage(img);
+        mapCanvas.drawImage(0, 0, img);
+        done = true;
+    }
+}
--- src/main/java/me/overlight/powertools/Plugin/PlInfo.java
+++ src/main/java/me/overlight/powertools/Plugin/PlInfo.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 
 public class PlInfo {
     public static final String PREFIX = ChatColor.translateAlternateColorCodes('&', "&6[&l&2Power&b&lToolS&r&6]&r&f ");
+    public static final String INV_PREFIX = ChatColor.translateAlternateColorCodes('&', "&l&2Power&b&lToolS&r&6 >> &r&f ");
     public static final String KICK_PREFIX = ChatColor.translateAlternateColorCodes('&', "&5 POWER TOOLS\n\\___________________/\n");
     public static final String VERSION = "1.3";
 
