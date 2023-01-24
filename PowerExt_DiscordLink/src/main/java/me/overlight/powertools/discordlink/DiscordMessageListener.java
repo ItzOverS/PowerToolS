663e1faf7dc6be54332fe8cf9365674f372cc681
ItzOver <alireza@daneshian.ir>
2023-01-24 05:28:33 +0330

    fixed channel checker to only one channel ( in config )

--- PowerExt_DiscordLink/src/main/java/me/overlight/powertools/discordlink/DiscordMessageListener.java
+++ PowerExt_DiscordLink/src/main/java/me/overlight/powertools/discordlink/DiscordMessageListener.java
@@ -4,6 +4,8 @@ import me.overlight.powertools.Plugin.PlInfo;
 import me.overlight.powertools.PowerTools;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.restaction.AuditableRestAction;
+import net.dv8tion.jda.internal.requests.restaction.AuditableRestActionImpl;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.jetbrains.annotations.NotNull;
@@ -18,28 +20,23 @@ public class DiscordMessageListener
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        PowerTools.INSTANCE.getServer().getConsoleSender().sendMessage("Called Ev 1");
+        if(!event.getChannel().getId().equals(PowerTools.config.getString(PowerExt.module.getConfigName()))) return;
         String m = event.getMessage().getContentDisplay();
         if(m.split(" ").length != 2) return;
         String code = m.split(" ")[1].trim();
         String author = event.getAuthor().getName();
-        PowerTools.INSTANCE.getServer().getConsoleSender().sendMessage("Called Ev 2");
         if(!m.startsWith(PowerExt.dsPrefix)) return;
         String mcUsername = getKey(code);
-        PowerTools.INSTANCE.getServer().getConsoleSender().sendMessage("Called Ev 3");
         if(mcUsername == null) return;
-        PowerTools.INSTANCE.getServer().getConsoleSender().sendMessage("Called Ev 4");
         if(Bukkit.getPlayer(mcUsername) == null){
             PowerExt.playerCodes.remove(mcUsername);
-            PowerTools.INSTANCE.getServer().getConsoleSender().sendMessage("Called Ev 5");
             return;
         }
         try {
-            PowerTools.INSTANCE.getServer().getConsoleSender().sendMessage("Called Ev 6");
-            event.getMessage().delete();
+            AuditableRestAction<Void> result = event.getMessage().delete();
             PowerExt.discordIDsUser.put(mcUsername, author);
             Bukkit.getPlayer(mcUsername).sendMessage(PlInfo.PREFIX + ChatColor.GREEN + "You has been simplify verified");
-        } catch(Exception e) { }
+        } catch(Exception ignored) { }
     }
 
     public String getKey(String value){
