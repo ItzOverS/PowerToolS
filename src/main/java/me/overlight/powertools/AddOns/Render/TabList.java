66729f395db1f01bb8be0ae5f75086f1ca75dd27
ItzOver <alireza@daneshian.ir>
2023-01-27 06:15:30 +0330

    fixed TabList for +1.13 & added NMS class reader

--- src/main/java/me/overlight/powertools/AddOns/Render/TabList.java
+++ src/main/java/me/overlight/powertools/AddOns/Render/TabList.java
@@ -1,7 +1,10 @@
 package me.overlight.powertools.AddOns.Render;
 
+import io.github.retrooper.packetevents.PacketEvents;
+import io.github.retrooper.packetevents.utils.server.ServerVersion;
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.overlight.powertools.AddOns.AddOn;
+import me.overlight.powertools.NMSSupport;
 import me.overlight.powertools.PowerTools;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -9,13 +12,16 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class TabList
         extends AddOn {
     public TabList() {
-        super("RenderAddOns.TabList", "1.0", "Make your tab costume", "NONE", PowerTools.config.getBoolean("RenderAddOns.TabList"));
+        super("RenderAddOns.TabList", "1.0", "Make your tab costume", "NONE", PowerTools.config.getBoolean("RenderAddOns.TabList.enabled"));
     }
 
     private static int index = 0;
@@ -29,21 +35,25 @@ public class TabList
                     footerString = ChatColor.translateAlternateColorCodes('&', mixArray(sec.getStringList("footer"), "\n"));
             for(Player player: Bukkit.getOnlinePlayers()) {
                 player.setPlayerListName(PlaceholderAPI.setPlaceholders(player, ChatColor.translateAlternateColorCodes('&', sec.getString("per-player"))));
-                net.minecraft.server.v1_8_R3.PacketPlayOutPlayerListHeaderFooter packet = new net.minecraft.server.v1_8_R3.PacketPlayOutPlayerListHeaderFooter();
-                Object header = new net.minecraft.server.v1_8_R3.ChatComponentText(PlaceholderAPI.setPlaceholders(player, headerString)),
-                        footer = new net.minecraft.server.v1_8_R3.ChatComponentText(PlaceholderAPI.setPlaceholders(player, footerString));
                 try {
-                    Field a = packet.getClass().getDeclaredField("a"),
-                            b = packet.getClass().getDeclaredField("b");
+                    Object packet = Objects.requireNonNull(NMSSupport.getClass("PacketPlayOutPlayerListHeaderFooter")).getConstructor().newInstance();
+                    Object header = Objects.requireNonNull(NMSSupport.getClass("ChatComponentText")).getConstructor(String.class).newInstance(PlaceholderAPI.setPlaceholders(player, headerString)),
+                            footer = Objects.requireNonNull(NMSSupport.getClass("ChatComponentText")).getConstructor(String.class).newInstance(PlaceholderAPI.setPlaceholders(player, footerString));
+                    Field a = packet.getClass().getDeclaredField((PacketEvents.get().getServerUtils().getVersion().isOlderThan(ServerVersion.v_1_13))? "a": "header"),
+                            b = packet.getClass().getDeclaredField((PacketEvents.get().getServerUtils().getVersion().isOlderThan(ServerVersion.v_1_13))? "b": "footer");
                     a.setAccessible(true);
                     b.setAccessible(true);
                     a.set(packet, header);
                     b.set(packet, footer);
-                    ((org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
-                } catch (NoSuchFieldException | IllegalAccessException ignored) {
+
+                    NMSSupport.sendPacket(player, packet);
+                } catch (NoSuchMethodException | IllegalAccessException | NoSuchFieldException |
+                         InvocationTargetException | InstantiationException e) {
+                    throw new RuntimeException(e);
                 }
             }
             index++;
+            if(index >= PowerTools.config.getConfigurationSection(this.getName() + ".lists").getKeys(false).size()) index = 0;
         }, 0, PowerTools.config.getInt(this.getName() + ".switchDelay"));
     }
 
