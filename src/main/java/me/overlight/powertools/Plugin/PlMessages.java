f5ca8488e74709abcfb021cf2bd9dc456635887e
ItzOver <alireza@daneshian.ir>
2023-01-30 17:53:19 +0330

    Update PlMessages.java

--- src/main/java/me/overlight/powertools/Plugin/PlMessages.java
+++ src/main/java/me/overlight/powertools/Plugin/PlMessages.java
@@ -2,46 +2,48 @@ package me.overlight.powertools.Plugin;
 
 import me.overlight.powertools.Libraries.ColorFormat;
 import me.overlight.powertools.Libraries.RepItem;
+import org.bukkit.ChatColor;
 
 public enum PlMessages {
 
-    PlayerNotFind(ColorFormat.formatColor("@color_redPlayer not find")),
-    KnockBack_SimplifyApplied(ColorFormat.formatColor("@color_greenSimplify applied knockback on @color_gold%PLAYER_NAME%")),
-    KnockBack_FailedToApply(ColorFormat.formatColor("@color_redI don't see valid knockback on @color_gold%PLAYER_NAME%")),
-    KnockBack_StickSimplifyGiven(ColorFormat.formatColor("@color_greenSimplify given @color_gold%PLAYER_NAME%@color_green, knockback stick")),
-    Freeze_StickSimplifyGiven(ColorFormat.formatColor("@color_greenSimplify given @color_gold%PLAYER_NAME%@color_green, freeze stick")),
-    Rotate_StickSimplifyGiven(ColorFormat.formatColor("@color_greenSimplify given @color_gold%PLAYER_NAME%@color_green, rotate stick")),
-    KnockBack_PlayerInBlock(ColorFormat.formatColor("@color_gold%PLAYER_NAME% @color_redis in block & i can't give they knockback")),
-    Freeze_TargetIsNowFrozen(ColorFormat.formatColor("@color_gold%PLAYER_NAME% @color_greenis now frozen")),
-    Freeze_TargetIsNoLongerFrozen(ColorFormat.formatColor("@color_gold%PLAYER_NAME% @color_redis no longer frozen")),
-    Protect_PlayerIsNowProtected(ColorFormat.formatColor("@color_gold%PLAYER_NAME% @color_greenis now protected")),
-    Protect_PlayerIsNoLongerProtected(ColorFormat.formatColor("@color_gold%PLAYER_NAME% @color_redis no longer protected")),
-    Protect_YouAreNowProtected(ColorFormat.formatColor("@color_greenYou are now protected")),
-    Protect_YouAreNoLongerProtected(ColorFormat.formatColor("@color_redYou are no longer protected")),
-    Rotate_FailedToRotate(ColorFormat.formatColor("@color_redFailed to rotate @color_gold%PLAYER_NAME%")),
-    Rotate_SimplifyRotated(ColorFormat.formatColor("@color_greenSimplify rotated @color_gold%PLAYER_NAME%")),
-    Vanish_PlayerIsNowVanish(ColorFormat.formatColor("@color_gold%PLAYER_NAME% @color_green is now vanish")),
-    Vanish_PlayerIsNoLongerVanish(ColorFormat.formatColor("@color_gold%PLAYER_NAME% @color_red is no longer vanish")),
-    Vanish_YouAreNowVanish(ColorFormat.formatColor("@color_greenYou're now vanish")),
-    Vanish_YouAreNoLongerVanish(ColorFormat.formatColor("@color_redYou're no longer vanish")),
-    ClDetector_PlayerJoinedUsing(ColorFormat.formatColor("@color_gold%PLAYER_NAME%@color_green joined using @color_red%CLIENT% %VERSION%")),
-    ClDetector_FailedToDetectClient(ColorFormat.formatColor("@color_redFailed to detect @color_gold%PLAYER_NAME%@color_red's client")),
-    CpsCheck_KickedForMaxCps(ColorFormat.formatColor("@color_gold%PLAYER_NAME% @color_red has kicked for max %TYPE% cps. cps: %CPS%")),
-    CpsCheck_UsingAutoClicker(ColorFormat.formatColor("@color_gold%PLAYER_NAME% @color_red using auto clicker")),
-    CpsCheck_KickedForAutoClicker(ColorFormat.formatColor("@color_gold%PLAYER_NAME% @color_red kicked for using auto clicker")),
-    NetworkChecker_PlayerJoinedUsing(ColorFormat.formatColor("@color_gold%PLAYER_NAME% @color_green's IP is %IP%")),
-    NetworkChecker_PlayerTempBannedForInvalidCountry(ColorFormat.formatColor("@color_gold%PLAYER_NAME%@color_green has temp banned for invalid country")),
-    Toggle_TargetItemNotFind(ColorFormat.formatColor("@color_redTarget type not find!")),
-    Toggle_SimplifySet(ColorFormat.formatColor("@color_greenSimplify toggled @color_gold%TARGET_PLAYER%@color_green's @color_gold%TARGET_ITEM%@color_green for You")),
-    NoPermission(ColorFormat.formatColor("@color_redYou don't have enough permission to do this!"))
+    PlayerNotFind("@color_redPlayer not find"),
+    KnockBack_SimplifyApplied("@color_greenSimplify applied knockback on @color_gold%PLAYER_NAME%"),
+    KnockBack_FailedToApply("@color_redI don't see valid knockback on @color_gold%PLAYER_NAME%"),
+    KnockBack_StickSimplifyGiven("@color_greenSimplify given @color_gold%PLAYER_NAME%@color_green, knockback stick"),
+    Freeze_StickSimplifyGiven("@color_greenSimplify given @color_gold%PLAYER_NAME%@color_green, freeze stick"),
+    Rotate_StickSimplifyGiven("@color_greenSimplify given @color_gold%PLAYER_NAME%@color_green, rotate stick"),
+    KnockBack_PlayerInBlock("@color_gold%PLAYER_NAME% @color_redis in block & i can't give they knockback"),
+    Freeze_TargetIsNowFrozen("@color_gold%PLAYER_NAME% @color_greenis now frozen"),
+    Freeze_TargetIsNoLongerFrozen("@color_gold%PLAYER_NAME% @color_redis no longer frozen"),
+    Protect_PlayerIsNowProtected("@color_gold%PLAYER_NAME% @color_greenis now protected"),
+    Protect_PlayerIsNoLongerProtected("@color_gold%PLAYER_NAME% @color_redis no longer protected"),
+    Protect_YouAreNowProtected("@color_greenYou are now protected"),
+    Protect_YouAreNoLongerProtected("@color_redYou are no longer protected"),
+    Rotate_FailedToRotate("@color_redFailed to rotate @color_gold%PLAYER_NAME%"),
+    Rotate_SimplifyRotated("@color_greenSimplify rotated @color_gold%PLAYER_NAME%"),
+    Vanish_PlayerIsNowVanish("@color_gold%PLAYER_NAME% @color_green is now vanish"),
+    Vanish_PlayerIsNoLongerVanish("@color_gold%PLAYER_NAME% @color_red is no longer vanish"),
+    Vanish_YouAreNowVanish("@color_greenYou're now vanish"),
+    Vanish_YouAreNoLongerVanish("@color_redYou're no longer vanish"),
+    ClDetector_PlayerJoinedUsing("@color_gold%PLAYER_NAME%@color_green joined using @color_red%CLIENT% %VERSION%"),
+    ClDetector_FailedToDetectClient("@color_redFailed to detect @color_gold%PLAYER_NAME%@color_red's client"),
+    CpsCheck_KickedForMaxCps("@color_gold%PLAYER_NAME% @color_red has kicked for max %TYPE% cps. cps: %CPS%"),
+    CpsCheck_UsingAutoClicker("@color_gold%PLAYER_NAME% @color_red using auto clicker"),
+    CpsCheck_KickedForAutoClicker("@color_gold%PLAYER_NAME% @color_red kicked for using auto clicker"),
+    CpsCheck_PlayersCpsGet("@color_gold%PLAYER_NAME% @color_green's @color_gold%CPS_TYPE%@color_green cps is %CPS%"),
+    NetworkChecker_PlayerJoinedUsing("@color_gold%PLAYER_NAME% @color_green's IP is %IP%"),
+    NetworkChecker_PlayerTempBannedForInvalidCountry("@color_gold%PLAYER_NAME%@color_green has temp banned for invalid country"),
+    Toggle_TargetItemNotFind("@color_redTarget type not find!"),
+    Toggle_SimplifySet("@color_greenSimplify toggled @color_gold%TARGET_PLAYER%@color_green's @color_gold%TARGET_ITEM%@color_green for You"),
+    NoPermission("@color_redYou don't have enough permission to do this!")
 
     ;
-    private final String name;
-    PlMessages(String name){
-        this.name = name;
+    private final String desc;
+    PlMessages(String desc){
+        this.desc = desc;
     }
     public String get(RepItem ... items){
-        String item = this.name;
+        String item = ColorFormat.formatColor(this.desc);
         for(RepItem i : items) item = item.replace(i.key(), i.value());
         return PlInfo.PREFIX + item;
     }
