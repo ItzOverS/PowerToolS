5c16052dd17898899a1d1dd56c6b76db0c567ee0
ItzOver <alireza@daneshian.ir>
2023-02-12 18:43:05 +0330

    fixed PLHider

--- src/main/java/me/overlight/powertools/spigot/AddOns/Server/PluginHider/PluginHider.java
+++ src/main/java/me/overlight/powertools/spigot/AddOns/Server/PluginHider/PluginHider.java
@@ -2,6 +2,7 @@ package me.overlight.powertools.spigot.AddOns.Server.PluginHider;
 
 import io.github.retrooper.packetevents.PacketEvents;
 import me.overlight.powertools.spigot.AddOns.AddOn;
+import me.overlight.powertools.spigot.PowerTools;
 import org.bukkit.event.Listener;
 
 public class PluginHider
@@ -9,7 +10,7 @@ public class PluginHider
         implements Listener {
 
     public PluginHider() {
-        super("ServerAddOns.PluginHider", "1.0", "Hide your server's plugins for plugin detector clients", true);
+        super("ServerAddOns.PluginHider", "1.0", "Hide your server's plugins for plugin detector clients", PowerTools.config.getBoolean("ServerAddOns.PluginHider.enabled"));
     }
 
     @Override
