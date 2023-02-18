f2991e76567298f42b08f037ebc3e4dc22aeac3c
ItzOver <alireza@daneshian.ir>
2023-02-18 13:34:21 +0330

    attempted to fix place holders ( failed )

--- src/main/java/me/overlight/powertools/spigot/Libraries/PlaceHolders.java
+++ src/main/java/me/overlight/powertools/spigot/Libraries/PlaceHolders.java
@@ -3,14 +3,16 @@ package me.overlight.powertools.spigot.Libraries;
 import io.github.retrooper.packetevents.PacketEvents;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.overlight.powertools.spigot.AddOns.Main.VersionCheck;
+import me.overlight.powertools.spigot.Plugin.PlInfo;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 public class PlaceHolders
         extends PlaceholderExpansion {
     @Override
     public String getIdentifier() {
-        return "PowerToolS";
+        return "powertools";
     }
 
     @Override
@@ -20,7 +22,7 @@ public class PlaceHolders
 
     @Override
     public String getVersion() {
-        return "1.0";
+        return PlInfo.VERSION;
     }
 
     @Override
@@ -41,10 +43,18 @@ public class PlaceHolders
                 return PacketEvents.get().getPlayerUtils().getClientVersion(p).toString().replace("v_", "").replace("_", ".");
             case "cl-brand":
                 return VersionCheck.playersClientBrand.get(p.getName());
-            case "player-name":
-                return p.getName();
-            case "player-display":
-                return p.getDisplayName();
+            case "onlines-size":
+                return "" + Bukkit.getOnlinePlayers().size();
+            case "max-onlines-size":
+                return "" + Bukkit.getServer().getMaxPlayers();
+        }
+        return null;
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer p, String params) {
+        if (p == null) return "";
+        switch (params.toLowerCase()) {
             case "onlines-size":
                 return "" + Bukkit.getOnlinePlayers().size();
             case "max-onlines-size":
