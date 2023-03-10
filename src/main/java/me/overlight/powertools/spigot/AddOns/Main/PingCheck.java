b91170ed75dc8aa9a2c5b44e40f00549f2235dd8
ItzOver <alireza@daneshian.ir>
2023-03-10 11:13:20 +0330

    Update PingCheck.java

--- src/main/java/me/overlight/powertools/spigot/AddOns/Main/PingCheck.java
+++ src/main/java/me/overlight/powertools/spigot/AddOns/Main/PingCheck.java
@@ -26,15 +26,11 @@ public class PingCheck
             if (PlayTime.PlayTime.get(player.getName()).minute > 5) {
                 return;
             }
-            Bukkit.getScheduler().runTaskAsynchronously(PowerTools.INSTANCE, new Runnable() {
-
-                @Override
-                public void run() {
-                    PowerTools.kick(player, PlInfo.KICK_PREFIX + ChatColor.RED + "Your ping is too high - " + PacketEvents.get().getPlayerUtils().getPing(player) + "ms > " + PowerTools.config.getInt(thisName + ".maxPing") + "ms");
-                    PowerTools.Alert(PowerTools.Target.STAFF, ChatColor.RED + player.getName() + ChatColor.GOLD + " kicked for high ping");
-                    if (PowerTools.config.getBoolean(thisName + ".alert-on-discord")) {
-                        DiscordAPI.sendEmbedOnWebhook(player.getName() + " kicked - HighPing", player.getName() + " kicked for has unstable ping on server");
-                    }
+            Bukkit.getScheduler().runTaskAsynchronously(PowerTools.INSTANCE, () -> {
+                PowerTools.kick(player, PlInfo.KICK_PREFIX + ChatColor.RED + "Your ping is too high - " + PacketEvents.get().getPlayerUtils().getPing(player) + "ms > " + PowerTools.config.getInt(thisName + ".maxPing") + "ms");
+                PowerTools.Alert(PowerTools.Target.STAFF, ChatColor.RED + player.getName() + ChatColor.GOLD + " kicked for high ping");
+                if (PowerTools.config.getBoolean(thisName + ".alert-on-discord")) {
+                    DiscordAPI.sendEmbedOnWebhook(player.getName() + " kicked - HighPing", player.getName() + " kicked for has unstable ping on server");
                 }
             });
         }
