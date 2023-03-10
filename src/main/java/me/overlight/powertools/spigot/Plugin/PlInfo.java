376772a1f98937dee289d283f8fc12ed493eab2a
ItzOver <alireza@daneshian.ir>
2023-03-10 11:10:47 +0330

    Update PlInfo.java

--- src/main/java/me/overlight/powertools/spigot/Plugin/PlInfo.java
+++ src/main/java/me/overlight/powertools/spigot/Plugin/PlInfo.java
@@ -8,12 +8,11 @@ public class PlInfo {
     public static final String KICK_PREFIX = ChatColor.translateAlternateColorCodes('&', "&5 POWER TOOLS\n\\___________________/\n");
     public static final String DUMP_STYLE =
             "Server version: %SERVER_VERSION%\n" +
-            "Plugin version: %PL_VERSION%\n" +
+                    "Plugin version: %PL_VERSION%\n" +
                     "Bungee Sync: %BUNGEE_CONNECTED%\n\n" +
                     "- Enabled AddOns: %ENABLED_ADDONS%\n" +
                     "- Enabled Extensions: %ENABLED_EXTENSIONS%\n" +
-                    "- Plugins: %PLUGINS%\n\n" +
-                    "Occurred Errors: %ERRORS%";
+                    "- Plugins: %PLUGINS%\n\n";
     public static final String VERSION = "1.5";
 
     public static final class ADDONS {
